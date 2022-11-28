ddbdcb3962f50ef4805e1f2448802fe94fc28fa0
fillip1984 <fillip1984@gmail.com>
2022-11-27 22:25:55 -0500

    working on points of interest console log statement, trying to line everything up

--- src/main/java/org/home/knowledge/sow/SecurityConfig.java
+++ src/main/java/org/home/knowledge/sow/SecurityConfig.java
@@ -1,81 +1,49 @@
 package org.home.knowledge.sow;
 
-import org.home.knowledge.sow.security.RsaKeyProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
-import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * Spring Security config
- * <p>
- * JWT config, see:
- * https://github.com/danvega/jwt/blob/master/src/main/java/dev/danvega/jwt/config/SecurityConfig.java
  * 
  */
 @Configuration
 @EnableWebSecurity
 @Slf4j
 public class SecurityConfig {
-    // Other Sources:
+
+    // TODO: someday, go back to Spring Boot 3 and try and implement JWT. here were
+    // some useful resources:
+    // https://github.com/danvega/jwt/blob/master/src/main/java/dev/danvega/jwt/config/SecurityConfig.java
     // https://github.com/spring-projects/spring-security-samples/tree/main/servlet/spring-boot/java/authentication/username-password/user-details-service/custom-user/src
     // https://github.com/spring-projects/spring-security-samples/blob/main/servlet/spring-boot/java/jwt/login/src/main/java/example/RestConfig.java
 
-    private final RsaKeyProperties jwtConfigProperties;
-
-    public SecurityConfig(RsaKeyProperties jwtConfigProperties) {
-        this.jwtConfigProperties = jwtConfigProperties;
-    }
-
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        log.warn(
-                "H2 database console is unsecured for development purposes. THIS CONFIGURATION SHOULD NOT MAKE IT INTO A QA OR PROD ENVIRONMENT!!!");
-        // @formatter:off
-        // See: https://www.baeldung.com/spring-cors
-        // H2 console required frameOptions be disabled, see: https://springframework.guru/using-the-h2-database-console-in-spring-boot-with-spring-security/
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .antMatchers("/home*/**", "/v3/api-docs*/**", "/actuator/health*/**").permitAll()
+                        .antMatchers("/h2-console*/**", "/actuator*/**", "/swagger-ui*/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults())
+                // See: https://www.baeldung.com/spring-cors#cors-with-spring-security
+                .cors(Customizer.withDefaults())
+                // See:
+                // https://springframework.guru/using-the-h2-database-console-in-spring-boot-with-spring-security/
+                .headers(headers -> headers.frameOptions().disable())
+                .exceptionHandling(ex -> ex.accessDeniedPage("/accessDenied.html"));
 
-        // disabled BearerTokenAuthenticationEntryPoint due to not always using Rest and it was breaking the browser
-        // might have to revisit this: https://www.jessym.com/articles/combining-spring-boot-auth-methods
-        http.cors(Customizer.withDefaults())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/h2-console/**", "/swagger-ui/**", "/v3/api-docs/**").hasAnyRole("USER", "ADMIN")
-                .requestMatchers("/actuator/**", "/admin/**").hasAnyRole("ADMIN")
-                .anyRequest().authenticated())
-            .formLogin(Customizer.withDefaults())
-            .csrf((csrf) -> csrf.ignoringRequestMatchers("/token"))
-            .httpBasic(Customizer.withDefaults())
-			.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-			.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .exceptionHandling((ex) -> ex
-                        // .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler()))
-            .headers().frameOptions().disable();
         return http.build();
-        // @formatter:on
     }
 
     @Bean
@@ -95,23 +63,6 @@ public class SecurityConfig {
                             .build();
         // @formatter:on
         return new InMemoryUserDetailsManager(user, admin);
-    }
-
-    @Bean
-    JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withPublicKey(jwtConfigProperties.publicKey()).build();
-    }
-
-    @Bean
-    JwtEncoder jwtEncoder() {
-        // @formatter:off
-        JWK jwk = new RSAKey.
-                        Builder(jwtConfigProperties.publicKey())
-                        .privateKey(jwtConfigProperties.privateKey())
-                        .build();
-        // @formatter:on
-        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
-        return new NimbusJwtEncoder(jwks);
     }
 
 }
