8379ca02438d73be3f3c5e79d9e575e7fcc36b56
fillip1984 <fillip1984@gmail.com>
2022-11-27 23:57:00 -0500

    cleaned up error pages and fixed a few misc/broken things since we relocated stati html to public folder

--- src/main/java/org/home/knowledge/sow/WebConfig.java
+++ src/main/java/org/home/knowledge/sow/WebConfig.java
@@ -2,7 +2,6 @@ package org.home.knowledge.sow;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.extern.slf4j.Slf4j;
