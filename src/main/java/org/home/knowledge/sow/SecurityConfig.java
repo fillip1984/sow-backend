54ae7b7d6da99e7c597dfbe4c1a4a8537733983e
fillip1984 <fillip1984@gmail.com>
2022-11-27 22:59:51 -0500

    security is working, prepping for requestMatchers and improving dx slightly

--- src/main/java/org/home/knowledge/sow/SecurityConfig.java
+++ src/main/java/org/home/knowledge/sow/SecurityConfig.java
@@ -32,8 +32,8 @@ public class SecurityConfig {
         // http://phillips-macbook-pro.local:7878/sow/h2-console/login.do?jsessionid=169664501c38e7fe9e26a442a19f3398
         http
                 .authorizeHttpRequests(auth -> auth
-                        .antMatchers("/", "/home/**", "/v3/api-docs/**", "/actuator/health/**").permitAll()
-                        .antMatchers("/h2-console/**", "/actuator/**", "/swagger-ui/**").hasAnyRole("ADMIN")
+                        .antMatchers("/", "/server-home*/**", "/v3/api-docs*/**", "/actuator/health*/**").permitAll()
+                        .antMatchers("/h2-console*/**", "/actuator*/**", "/swagger-ui*/**").hasAnyRole("ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults())
--- src/main/java/org/home/knowledge/sow/controller/spec/HomeController.java
+++ src/main/java/org/home/knowledge/sow/controller/spec/HomeController.java
@@ -1,6 +1,5 @@
 package org.home.knowledge.sow.controller.spec;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +16,4 @@ public class HomeController {
         log.info("Welcome home");
         return "server-index.html";
     }
-
-    @GetMapping("/json")
-    public ResponseEntity<String> helloFromJson() {
-        log.info("Welcoming from json response");
-        return ResponseEntity.ok("Welcome home");
-    }
-
 }
