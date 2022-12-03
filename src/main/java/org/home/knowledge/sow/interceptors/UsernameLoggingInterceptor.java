0560b0636f2737d61a0a9dfd08121ad1db8c1661
fillip1984 <fillip1984@gmail.com>
2022-12-03 11:30:47 -0500

    documenting

--- src/main/java/org/home/knowledge/sow/interceptors/UsernameLoggingInterceptor.java
+++ src/main/java/org/home/knowledge/sow/interceptors/UsernameLoggingInterceptor.java
@@ -11,6 +11,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * Username logging interceptor allows for us to add usernames to logs
+ * <p>
+ * We are adding/removing username to logging MDC (Mapped Diagnostic Context
+ * which is fancy
+ * talk for we'll add something to the thread.local so that it stays in scope
+ * the entire time the thread is in used) so that when we log we have the
+ * current user's username added to the log
+ */
 @Component
 public class UsernameLoggingInterceptor implements HandlerInterceptor {
 
